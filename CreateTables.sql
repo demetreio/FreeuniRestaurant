6a9bdbf38caf3bedea8214a7b8e9ba44306d8029
zurachakve <zurachakve@gmail.com>
2012-12-25 22:20:07 +0400

    davamate sql faili romelic qmnis Table cxrils

---
+++ CreateTables.sql
@@ -0,0 +1,8 @@
+-- chemtan baza aris mysql, anu ro shedixar sul tavidan "use mysql" unda chawero. paroli ar adevs. 
+--copy from vincxa
+
+use mysql;
+
+CREATE TABLE Tables ( id INT NOT NULL AUTO_INCREMENT, size int NOT NULL, description(varchar(100), PRIMARY KEY (id));
+-- id aris magidis identifikatori, size არის მაგიდის სიდიდე  
+
