b879432071959b7ae27f14d34500ee1c82d2cddc
Nodar Dzamukashvili <ndzam10@freeuni.edu.ge>
2012-12-25 13:58:55 +0400

    Table class

---
+++ src/Table.java
@@ -0,0 +1,28 @@
+
+public class Table {
+	
+	private int id;
+	private int size;
+	private String description;
+	
+	public Table(int id, int size, String description){
+		this.id = id;
+		this.size = size;
+		this.description = description;
+	}
+	
+	public int getId(){
+		return id;
+		
+	}
+	
+	public int getSize(){
+		return size;
+		
+	}
+	
+	public String getDescription(){
+		return description;
+		
+	}
+}
