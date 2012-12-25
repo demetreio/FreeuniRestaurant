a432ebe16a78938032605def90edfefa32d4b329
Nodar Dzamukashvili <ndzam10@freeuni.edu.ge>
2012-12-25 14:48:42 +0400

    TableInfo class

--- src/TableInfo.java
+++ src/TableInfo.java
@@ -1,9 +1,24 @@
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 
 public class TableInfo {
 	
+	
 	public ArrayList<Table> getAllTables(){
-		return null;
+		DBConnector dbc = DBConnector.getInstance();
+		ResultSet rs = dbc.getTables();
+		ArrayList<Table> list = new ArrayList<Table>();
+		try {
+			while(rs.next()){
+				Table t = new Table(rs.getInt(1), rs.getInt(2), rs.getString(3));
+				list.add(t);
+			}
+		} catch (SQLException e) {
+			// TODO ??????
+			e.printStackTrace();
+		}
+		return list;
 	}
 }
