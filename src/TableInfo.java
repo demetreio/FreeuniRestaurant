d55176e0f725cca838ae1dbe31328644da0241d9
Nodar Dzamukashvili <ndzam10@freeuni.edu.ge>
2012-12-25 19:45:19 +0400

    შესწორებული tableInfo

--- src/TableInfo.java
+++ src/TableInfo.java
@@ -6,18 +6,13 @@ import java.util.ArrayList;
 public class TableInfo {
 	
 	
-	public ArrayList<Table> getAllTables(){
+	public ArrayList<Table> getAllTables() throws SQLException{
 		DBConnector dbc = DBConnector.getInstance();
 		ResultSet rs = dbc.getTables();
 		ArrayList<Table> list = new ArrayList<Table>();
-		try {
-			while(rs.next()){
-				Table t = new Table(rs.getInt(1), rs.getInt(2), rs.getString(3));
-				list.add(t);
-			}
-		} catch (SQLException e) {
-			// TODO ??????
-			e.printStackTrace();
+		while(rs.next()){
+			Table t = new Table(rs.getInt(1), rs.getInt(2), rs.getString(3));
+			list.add(t);
 		}
 		return list;
 	}
