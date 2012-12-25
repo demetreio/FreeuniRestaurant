6d974ce4e9a60cc220ff78f1a116d0e5a6154d60
llaki <llaki10@freeuni.edu.ge>
2012-12-25 15:12:33 +0400

    Database tables created for reserved tables.
    შიგნით წერია ცხრილი შექმნის კოდი დაჯავშნული მაგიდებისთვის. ყურადღება
    მიაქციეთ, რომ ჩემთან ბაზის სახელი არის mysql, ანუ სულ თავიდან use mysql
    უნდა ჩაიწეროს გამოყენება როცა გინდა.

---
+++ CreateReservedTables.sql
@@ -0,0 +1,8 @@
+-- chemtan baza aris mysql, anu ro shedixar sul tavidan "use mysql" unda chawero. paroli ar adevs. 
+
+use mysql;
+
+CREATE TABLE ReservedTables ( id INT, reserveInfo VARCHAR(100) ); // id aris magidis identifikatori, xolo reservInfo aris 
+-- 30 sigrdzis bitstringi, sadac inaxeba ori dgis informacia, titoshi 15 saatia. 1 weria im saatis shesabamis indeqsze, roca magida
+-- aris dakavebuli.
+
