918894fd3fba38c4e8d88942b97753187abc21d2
giorgi kervalishvili <gkerv10@freeuni.edu.ge>
2012-12-28 22:21:26 +0400

    titqmis bolomdea

--- src/ge/edu/freeuni/restaurant/model/LoginServlet.java
+++ src/ge/edu/freeuni/restaurant/model/LoginServlet.java
@@ -1,9 +1,6 @@
 package ge.edu.freeuni.restaurant.model;
 
-
 import java.io.IOException;
-
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,16 +8,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class AccountCreatingServlet
+ * Servlet implementation class LoginServlet
  */
-@WebServlet("/AccountCreatingServlet")
-public class AccountCreatingServlet extends HttpServlet {
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public AccountCreatingServlet() {
+    public LoginServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -36,20 +33,7 @@ public class AccountCreatingServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String	user = request.getParameter("username");
-		String	pass = request.getParameter("myPass");
-		String	info = request.getParameter("info");
-		String 	name = request.getParameter("name");
-		String	surname =request.getParameter("surname");
-		if(user.equals("") || pass.equals("")){
-			RequestDispatcher dispatch = request.getRequestDispatcher("FillFields.html");
-			dispatch.forward(request, response);
-		}
-		else{
-			RequestDispatcher dispatch = request.getRequestDispatcher("AccountCreated.jsp");
-			dispatch.forward(request, response);
-		}
-		
+		// TODO Auto-generated method stub
 	}
 
 }
