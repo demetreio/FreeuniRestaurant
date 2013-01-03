24b42c47ac7de6d924c964c067c439a9b5117500
gkiko <gkiko10@freeuni.edu.ge>
2013-01-03 11:33:22 +0300

    ianxavs romelma userma romeli magida ra dros dajavshna

---
+++ CreateUserTable.sql
@@ -0,0 +1,3 @@
+--ianxavs romelma userma romeli magida ra dros dajavshna
+
+CREATE TABLE USER_TABLE ( username varchar(50) NOT NULL, id INT NOT NULL, reserveInfo VARCHAR(100), PRIMARY KEY (username,id));
