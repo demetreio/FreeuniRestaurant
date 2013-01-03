020ea04c245c42224a7721b621f808933788d6a9
gkiko <gkiko10@freeuni.edu.ge>
2013-01-03 11:40:49 +0300

    bazas ukavshirdeba es klasi da igebs magidis inpormacias

--- src/ge/edu/freeuni/restaurant/logic/TableInfo.java
+++ src/ge/edu/freeuni/restaurant/logic/TableInfo.java
@@ -8,27 +8,27 @@ public class TableInfo {
 	
 	
 	public ArrayList<Table> getAllTables() throws SQLException{
-//		DBConnector dbc = DBConnector.getInstance();
-//		ResultSet rs = null;
-//		try {
-//			rs = dbc.getTables();
-//		} catch (SQLException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
+		DBConnector dbc = DBConnector.getInstance();
+		ResultSet rs = null;
+		try {
+			rs = dbc.getTables();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		ArrayList<Table> list = new ArrayList<Table>();
-//		try {
-//			while(rs.next()){
-//				Table t = new Table(rs.getInt(1), rs.getInt(2), rs.getString(3));
-//				list.add(t);
-//			}
-//		} catch (SQLException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
+		try {
+			while(rs.next()){
+				Table t = new Table(rs.getInt(1), rs.getInt(2), rs.getString(3));
+				list.add(t);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
-		list.add(new Table(0, 6, "kvazimodo"));
-		list.add(new Table(1, 4, "safixvnos tavi"));
+//		list.add(new Table(0, 6, "kvazimodo"));
+//		list.add(new Table(1, 4, "safixvnos tavi"));
 		
 		return list;
 	}
