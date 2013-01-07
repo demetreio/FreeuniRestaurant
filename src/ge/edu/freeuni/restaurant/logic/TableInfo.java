37810e6a2390e8ea0724e6cfaa84c2709ad17bae
gkiko <gkiko10@freeuni.edu.ge>
2013-01-07 16:27:02 +0300

    nodaras versia. mushaobs cariel occupation cxrilze. bug jer ar gasworebula

--- src/ge/edu/freeuni/restaurant/logic/TableInfo.java
+++ src/ge/edu/freeuni/restaurant/logic/TableInfo.java
@@ -1,13 +1,12 @@
 package ge.edu.freeuni.restaurant.logic;
+
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-
 public class TableInfo {
-	
-	
-	public ArrayList<Table> getAllTables() throws SQLException{
+
+	public ArrayList<Table> getAllTables() throws SQLException {
 		DBConnector dbc = DBConnector.getInstance();
 		ResultSet rs = null;
 		try {
@@ -18,7 +17,7 @@ public class TableInfo {
 		}
 		ArrayList<Table> list = new ArrayList<Table>();
 		try {
-			while(rs.next()){
+			while (rs.next()) {
 				ResultSet rs2 = null;
 				try {
 					rs2 = dbc.getOccupiedBy(rs.getInt(1));
@@ -26,25 +25,28 @@ public class TableInfo {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-				boolean b;
+				boolean b = false;
 				String occupant = "";
-				if(rs2.next()){
-					b = true;
-					occupant = rs2.getString(2);
-				} else {
-					b = false;
+				try {
+					if (rs2.next()) {
+						b = true;
+						occupant = rs2.getString(2);
+					}
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
 				}
-				Table t = new Table(rs.getInt(1), rs.getInt(2), rs.getString(3), b, occupant);
+				Table t = new Table(rs.getInt(1), rs.getInt(2),
+						rs.getString(3), b, occupant);
 				list.add(t);
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		// list.add(new Table(0, 6, "kvazimodo"));
+		// list.add(new Table(1, 4, "safixvnos tavi"));
 
-//		list.add(new Table(0, 6, "kvazimodo"));
-//		list.add(new Table(1, 4, "safixvnos tavi"));
-		
 		return list;
 	}
 }
