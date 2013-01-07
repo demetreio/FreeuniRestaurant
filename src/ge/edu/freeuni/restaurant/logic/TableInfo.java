f4f3b78c95314aa6112cd0fcc6efbe8f86be8cd4
gkiko <gkiko10@freeuni.edu.ge>
2013-01-07 17:30:43 +0300

    იღებს ჯოინის შედეგს და table კლასს ქმინის. kidev erti bugi gaichita aq: joini 2 shedegs mibrunebs chems cmd-shi. lisi ki 1 zomis gamodis. tvini mome*** da tavi davanebe

--- src/ge/edu/freeuni/restaurant/logic/TableInfo.java
+++ src/ge/edu/freeuni/restaurant/logic/TableInfo.java
@@ -9,44 +9,51 @@ public class TableInfo {
 	public ArrayList<Table> getAllTables() throws SQLException {
 		DBConnector dbc = DBConnector.getInstance();
 		ResultSet rs = null;
-		try {
-			rs = dbc.getTables();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		rs = dbc.TableJoinOccupation();
 		ArrayList<Table> list = new ArrayList<Table>();
-		try {
-			while (rs.next()) {
-				ResultSet rs2 = null;
-				try {
-					rs2 = dbc.getOccupiedBy(rs.getInt(1));
-				} catch (SQLException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				boolean b = false;
-				String occupant = "";
-				try {
-					if (rs2.next()) {
-						b = true;
-						occupant = rs2.getString(2);
-					}
-				} catch (SQLException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				Table t = new Table(rs.getInt(1), rs.getInt(2),
-						rs.getString(3), b, occupant);
-				list.add(t);
+		
+		while(rs.next()){
+			int tableId = rs.getInt(1);
+			int tableSize = rs.getInt(2);
+			String description = rs.getString(3);
+			int tableId1 = rs.getInt(4);
+			String occupantId = rs.getString(5);
+			boolean b = false;
+			if(tableId == tableId1){
+				b = true;
 			}
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			Table t = new Table(tableId, tableSize, description, b, occupantId);
+			list.add(t);
 		}
-		// list.add(new Table(0, 6, "kvazimodo"));
-		// list.add(new Table(1, 4, "safixvnos tavi"));
-
+//		try {
+//			while (rs.next()) {
+//				ResultSet rs2 = null;
+//				try {
+//					rs2 = dbc.getOccupiedBy(rs.getInt(1));
+//				} catch (SQLException e) {
+//					// TODO Auto-generated catch block
+//					e.printStackTrace();
+//				}
+//				boolean b = false;
+//				String occupant = "";
+//				try {
+//					if (rs2.next()) {
+//						b = true;
+//						occupant = rs2.getString(2);
+//					}
+//				} catch (SQLException e) {
+//					// TODO Auto-generated catch block
+//					e.printStackTrace();
+//				}
+//				Table t = new Table(rs.getInt(1), rs.getInt(2),
+//						rs.getString(3), b, occupant);
+//				list.add(t);
+//			}
+//		} catch (SQLException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+		System.out.println(list.size());
 		return list;
 	}
 }
