5fe01234236b46e7532171c5f1f53f4794323651
Nodar Dzamukashvili <ndzam10@freeuni.edu.ge>
2013-01-10 13:23:30 +0400

    carieli menius da kerdzis klasi shevqmeni

---
+++ src/ge/edu/freeuni/restaurant/logic/Kerdzi.java
@@ -0,0 +1,5 @@
+package ge.edu.freeuni.restaurant.logic;
+
+public class Kerdzi {
+
+}
---
+++ src/ge/edu/freeuni/restaurant/logic/Menu.java
@@ -0,0 +1,11 @@
+package ge.edu.freeuni.restaurant.logic;
+
+import java.util.ArrayList;
+
+public class Menu {
+	
+	public ArrayList<Kerdzi> getMenu(){
+		
+		return null;
+	}
+}
