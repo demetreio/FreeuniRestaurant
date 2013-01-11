bc620b417a96cde19aeed6ac993e8ee243033ee4
Nodar Dzamukashvili <ndzam10@freeuni.edu.ge>
2013-01-11 18:12:58 +0400

    menu class

--- src/ge/edu/freeuni/restaurant/logic/Kerdzi.java
+++ src/ge/edu/freeuni/restaurant/logic/Kerdzi.java
@@ -2,7 +2,7 @@ package ge.edu.freeuni.restaurant.logic;
 
 public class Kerdzi {
 	
-	public Kerdzi(String name, double price, String saxeoba){
+	public Kerdzi(int id, String name, double price, String saxeoba){
 		
 	}
 }
