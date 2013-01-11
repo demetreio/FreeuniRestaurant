95f2a2f88fdec3aa226c3db42a55614c27baf0f1
Nodar Dzamukashvili <ndzam10@freeuni.edu.ge>
2013-01-11 18:09:18 +0400

    menu-s metodi shevavse

--- src/ge/edu/freeuni/restaurant/logic/Kerdzi.java
+++ src/ge/edu/freeuni/restaurant/logic/Kerdzi.java
@@ -1,5 +1,8 @@
 package ge.edu.freeuni.restaurant.logic;
 
 public class Kerdzi {
-
+	
+	public Kerdzi(String name, double price, String saxeoba){
+		
+	}
 }
--- src/ge/edu/freeuni/restaurant/logic/Menu.java
+++ src/ge/edu/freeuni/restaurant/logic/Menu.java
@@ -1,11 +1,19 @@
 package ge.edu.freeuni.restaurant.logic;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class Menu {
 	
-	public ArrayList<Kerdzi> getMenu(){
-		
-		return null;
+	public ArrayList<Kerdzi> getMenu() throws SQLException{
+		DBConnector db = DBConnector.getInstance();
+		ResultSet rs = db.selectFromMenu();
+		ArrayList<Kerdzi> list = new ArrayList<Kerdzi>();
+		while(rs.next()){
+			Kerdzi k = new Kerdzi(rs.getString(1), rs.getDouble(2), rs.getString(3));
+			list.add(k);
+		}
+		return list;
 	}
 }
