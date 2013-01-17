2e7ffa23d2273a76346edb2476fbc57a00587b80
Nodar Dzamukashvili <ndzam10@freeuni.edu.ge>
2013-01-18 00:54:13 +0400

    classi daemata

---
+++ src/ge/edu/freeuni/restaurant/logic/EditMenu.java
@@ -0,0 +1,18 @@
+package ge.edu.freeuni.restaurant.logic;
+
+public class EditMenu {
+	
+	public Kerdzi getObjectOfKerdzi(String name){
+		Kerdzi k = null;
+		
+		return k;
+	}
+	
+	public void saveInDbKerdzi(String name, double price, String category){
+		
+	}
+	
+	public void removeFromDbKerdzi(String name){
+		
+	}
+}
