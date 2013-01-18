31d134722f3a1e3b0ec3a6517a1e37c27996c36b
gkiko <gkiko10@freeuni.edu.ge>
2013-01-19 02:31:09 +0300

    default value chaemata

--- CreateReservedTables.sql
+++ CreateReservedTables.sql
@@ -2,7 +2,7 @@
 
 use mysql;
 
-CREATE TABLE ReservedTables ( id INT, reserveInfo VARCHAR(100), PRIMARY KEY (id)); // id aris magidis identifikatori, xolo reservInfo aris 
+CREATE TABLE ReservedTables ( id INT, reserveInfo VARCHAR(100) default "000000000000000000000000000000", PRIMARY KEY (id)); // id aris magidis identifikatori, xolo reservInfo aris 
 -- 30 sigrdzis bitstringi, sadac inaxeba ori dgis informacia, titoshi 15 saatia. 1 weria im saatis shesabamis indeqsze, roca magida
 -- aris dakavebuli.
 
