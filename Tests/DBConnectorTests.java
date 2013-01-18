e8cfbda6f9f161b58c33f5b1aeba3fdccb0b5d32
nikusha barbaqadze <nbarb09@freeuni.edu.ge>
2013-01-18 23:11:30 +0300

    Junit testebi 2 metodze

--- Tests/DBConnectorTests.java
+++ Tests/DBConnectorTests.java
@@ -1,7 +1,7 @@
 import ge.edu.freeuni.restaurant.logic.DBConnector;
 
 import java.sql.*;
-
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -17,7 +17,7 @@ public class DBConnectorTests {
 	@Before
 	public void setUp() throws ClassNotFoundException, SQLException{
 		db = DBConnector.getInstance();
-		
+		db.setUpTables();
 		
 	}
 	
@@ -26,7 +26,29 @@ public class DBConnectorTests {
 	public void testOne() throws ClassNotFoundException, SQLException {
 		db.insertIntoShekveta("dzamuka", 1, 3);
 		ResultSet rs = db.selectFromShekvetaByUserName("dzamuka");
+		while(rs.next()){
+			String name = rs.getString("username");
+			int kerdzi_id = Integer.parseInt(rs.getString("kerdzi_id"));
+			int quantity = Integer.parseInt(rs.getString("quantity"));
+			Assert.assertEquals(name, "dzamuka");
+			Assert.assertEquals(kerdzi_id, 1);
+			Assert.assertEquals(quantity, 3);
+			
+		}
 		
+		 db.insertIntoShekveta("nikusha", 5, 9);
+		 rs = db.selectFromShekvetaByUserName("nikusha");
+		while(rs.next()){
+			String name = rs.getString("username");
+			int kerdzi_id = Integer.parseInt(rs.getString("kerdzi_id"));
+			int quantity = Integer.parseInt(rs.getString("quantity"));
+			Assert.assertFalse(name.equals( "dzamuka"));
+			Assert.assertTrue(name.equals( "nikusha"));
+			Assert.assertEquals(kerdzi_id, 5);
+			Assert.assertEquals(quantity, 9);
+		
+		}
+
 		
 	}
 	
