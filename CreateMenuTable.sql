9029457374ff422f825de94b868bf537347af989
nikusha barbaqadze <nbarb09@freeuni.edu.ge>
2013-01-18 15:55:53 +0300

    menu tables_s auto_increment ის დამატება

--- CreateMenuTable.sql
+++ CreateMenuTable.sql
@@ -1,4 +1,4 @@
 -- inaxavs menus. 
 -- menu masshi shemavali kerdzebi, tavianti id-ebit, saxelebit, fasita da saxeobit.
 
-CREATE TABLE MENU (id INT NOT NULL, name VARCHAR(100) NOT NULL, price DOUBLE NOT NULL, foodtype VARCHAR(50) NOT NULL);
+CREATE TABLE MENU (id INT NOT NULL AUTO_INCREMENT, name VARCHAR(100) NOT NULL, price DOUBLE NOT NULL, foodtype VARCHAR(50) NOT NULL, PRIMARY KEY (id));
