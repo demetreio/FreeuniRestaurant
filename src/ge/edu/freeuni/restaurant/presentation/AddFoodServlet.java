65a824dabf57eac1c27344482d1cd741687fcda5
Kote Ujmajuridze <kujma10@freeuni.edu.ge>
2013-01-23 17:25:44 +0400

    servletis saxelis cvlileba
    P.S calk-calke vakomiteb ragaceebs tore davitanje sul konfliktebia

--- src/ge/edu/freeuni/restaurant/presentation/AddFoodServlet.java
+++ src/ge/edu/freeuni/restaurant/presentation/AddFoodServlet.java
@@ -14,14 +14,14 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class EditMenuServlet
  */
-@WebServlet("/EditMenuServlet")
-public class EditMenuServlet extends HttpServlet {
+@WebServlet("/AddFoodServlet")
+public class AddFoodServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public EditMenuServlet() {
+    public AddFoodServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
