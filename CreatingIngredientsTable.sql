c46c68f3381cf2d75cac81d52e30efb138045cb4
giorgi kervalishvili <gkerv10@freeuni.edu.ge>
2013-01-24 17:11:52 +0400

    tablis shqmna bazasi
    ingredients table shevkmeni

---
+++ CreatingIngredientsTable.sql
@@ -0,0 +1,3 @@
+//qmnis ingredient tables sadac sheianxaven romeli kerdziriti mzaddeba da ra chirdeba 
+
+Create table ingredients (product_id int not null,ingredeint_id int not null,unit varchar(300),quant double);
