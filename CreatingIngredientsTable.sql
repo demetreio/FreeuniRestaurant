10013e95a4860fc518520199abc28c06acbcac78
nikusha barbaqadze <nbarb09@freeuni.edu.ge>
2013-01-24 18:27:25 +0300

    saxlis shecva

--- CreatingIngredientsTable.sql
+++ CreatingIngredientsTable.sql
@@ -1,5 +1,5 @@
 //qmnis ingredient tables sadac sheianxaven romeli kerdziriti mzaddeba da ra chirdeba 
 
-Create table ingredients (product_id int not null,ingredeint_id int not null,unit varchar(300),quant double);
+Create table ingredients (product_id int not null,ingredient_id int not null,unit varchar(300),quant double);
 
 Create table ingsNames (ingredient_id int,ingredient_name varchar(400),PRIMARY KEY (ingredient_id));
