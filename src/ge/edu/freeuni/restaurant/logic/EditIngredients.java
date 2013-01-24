32857446fbdc1a096b778f46da4e675d0e195cd0
Nodar Dzamukashvili <ndzam10@freeuni.edu.ge>
2013-01-24 20:21:29 +0400

    editIngredient class daemata

---
+++ src/ge/edu/freeuni/restaurant/logic/EditIngredients.java
@@ -0,0 +1,17 @@
+package ge.edu.freeuni.restaurant.logic;
+
+public class EditIngredients {
+	
+	public void deleteEngredient(int kerdzisId, String ingredientName){
+		
+	}
+	
+	public void changeQuantity(int kerdzisId, String ingredientName, double newQuantity){
+		
+	}
+	
+	public void addIngredient(int kerdzisId, String ingredientName, double quantity, String unit){
+		
+	}
+	
+}
