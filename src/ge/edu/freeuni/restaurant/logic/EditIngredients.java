3a2aca1f684c01a3b15c54a1c4196bbc01f15764
Nodar Dzamukashvili <ndzam10@freeuni.edu.ge>
2013-01-24 20:52:44 +0400

    EditIngredients classi shevavse

--- src/ge/edu/freeuni/restaurant/logic/EditIngredients.java
+++ src/ge/edu/freeuni/restaurant/logic/EditIngredients.java
@@ -3,15 +3,18 @@ package ge.edu.freeuni.restaurant.logic;
 public class EditIngredients {
 	
 	public void deleteEngredient(int kerdzisId, String ingredientName){
-		
+		DBConnector db = DBConnector.getInstance();
+		db.deleteFromIngredients(kerdzisId, ingredientName);
 	}
 	
 	public void changeQuantity(int kerdzisId, String ingredientName, double newQuantity){
-		
+		DBConnector db = DBConnector.getInstance();
+		db.changeQuantityIntoIngredients(kerdzisId, ingredientName, newQuantity);
 	}
 	
 	public void addIngredient(int kerdzisId, String ingredientName, double quantity, String unit){
-		
+		DBConnector db = DBConnector.getInstance();
+		db.insertIntoIngredients(kerdzisId, ingredientName, quantity, unit);
 	}
 	
 }
