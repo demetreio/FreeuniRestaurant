e58088ddf56f7402662769101ffb17a3425f2c34
gkiko <gkiko10@freeuni.edu.ge>
2013-01-25 23:23:19 +0300

    satesto paili

---
+++ src/ge/edu/freeuni/restaurant/logic/MyThread.java
@@ -0,0 +1,39 @@
+package ge.edu.freeuni.restaurant.logic;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.AddressException;
+
+class MyThread extends Thread{		
+	private Date time;
+	private String userName;
+	private String emailAddress;
+	public MyThread(Date time, String userName, String emailAddress){
+		this.time=time;
+		this.userName=userName;
+		this.emailAddress=emailAddress;
+	}
+	
+	@Override
+	public void run(){
+		try {
+			long wait = time.getTime();
+			System.out.println(wait);
+			this.sleep(Calendar.getInstance().getTimeInMillis() - wait);
+			MailSender.sendRemainderMail(time, userName, emailAddress);
+		} catch (AddressException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (MessagingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+}
